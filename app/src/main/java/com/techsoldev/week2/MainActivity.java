ea9cb42fd7cfe648b7c5dd2e329656eac6e9dae9
F180243 <f180243@nu.edu.pk>
2021-09-25 03:15:02 +0500

    Java code complete (Final commit)

--- app/src/main/java/com/techsoldev/week2/MainActivity.java
+++ app/src/main/java/com/techsoldev/week2/MainActivity.java
@@ -1,18 +1,27 @@
 package com.techsoldev.week2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.text.method.PasswordTransformationMethod;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText password;
+    private EditText nameEdittext, passwordEdittext;
+    private Button loginBtn;
+    String name = "f180243";
+    String password = "1928";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +30,56 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        password = (EditText) findViewById(R.id.password_edittxt);
-        Toast.makeText(this, "Submitted", Toast.LENGTH_SHORT).show();
+        nameEdittext = (EditText) findViewById(R.id.name_edittext);
+        passwordEdittext = (EditText) findViewById(R.id.password_edittext);
 
-        //password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-
-        //password.setSelection(password.length());
-
-       // password.setTransformationMethod(new PasswordTransformationMethod());
+        loginBtn = (Button) findViewById(R.id.login_btn);
 
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (nameEdittext.getText().toString().trim().equalsIgnoreCase("")) {
+                    nameEdittext.setError("This field can not be blank");
+                    nameEdittext.requestFocus();
+                }
+                else if (passwordEdittext.getText().toString().trim().equalsIgnoreCase("")) {
+                    passwordEdittext.setError("This field can not be blank");
+                    passwordEdittext.requestFocus();
+                } else {
+                    loginFun();
+                }
+            }
+
+        });
+    }
+
+    private void loginFun() {
+
+        String nametext = nameEdittext.getText().toString();
+        String passwordtext = passwordEdittext.getText().toString();
+
+        if (nametext.equals(name)) {
+            if (passwordtext.equals(password)) {
+                Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+
+
+            } else {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Login failed")
+                        .setMessage("incorrect password")
+                        .setPositiveButton(android.R.string.ok, null)
+                        .show();
+            }
+        } else {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Login failed")
+                    .setMessage("incorrect name")
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show();
+        }
 
     }
+
 
 }
